dcc8261e3b5575e5e967911a40f3fb5680bb2467
HimankK2109 <himanksingh1094@gmail.com>
2024-11-16 20:33:20 +0530

    Updated Scraper

--- scraper/main.py
+++ scraper/main.py
@@ -160,12 +160,13 @@ products = [
 
 # Setup Chrome options to run headless
 chrome_options = Options()
+chrome_options.binary_location = "/usr/bin/google-chrome"
 chrome_options.add_argument('--headless')        # Run in headless mode (no GUI)
 chrome_options.add_argument('--no-sandbox')     # Disable sandboxing (for security)
 chrome_options.add_argument('--disable-dev-shm-usage')  # To avoid some memory issues
 chrome_options.add_argument('--disable-gpu')
 
-service = Service(ChromeDriverManager().install())
+service = Service('/usr/bin/chromedriver')
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
 # Max number of requests
